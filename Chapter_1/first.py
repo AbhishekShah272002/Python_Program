0be18c69630705dee9f5c6fbf462eaf8b8d4083a
Abhishek Shah <abhishek27shah@gmail.com>
2025-05-08 16:47:24 +0530

    Add files via upload

---
+++ Chapter_1/first.py
@@ -0,0 +1,11 @@
+# Python is a programming language 
+# Python is a high-level, interpreted language
+# Python is known for its readability and simplicity
+# Python is widely used in web development, data analysis, artificial intelligence, and more
+# Python supports multiple programming paradigms, including procedural, object-oriented, and functional programming
+# This is a comment 
+
+print("Hello World! ") # Printing Hello World
+print("Welcome to Python! ") # Printing Welcome to Python
+print("Python is a versatile language! ") # Printing Python is a versatile language
+print("Python is easy to learn! ") # Printing Python is easy to learn
---
+++ Chapter_1/module.py
@@ -0,0 +1,13 @@
+# This is a simple script to print a joke using the pyjokes module
+# import is used to include the pyjokes module in the script
+# The pyjokes module is a Python library that provides a collection of jokes
+import pyjokes
+
+# This is a single line comment
+""" This is a multi-line comment"""
+
+# print("Printing a joke using pyjokes module")
+"""
+# This is a simple script to print a joke using the pyjokes module"""
+joke = pyjokes.get_joke()
+print(joke)
