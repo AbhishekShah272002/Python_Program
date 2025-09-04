09e5b2dfa7464419f8b204f529ccfa054ec6cea1
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-04 20:37:23 +0530

    Create 02_Problem.py

---
+++ Practice_4/02_Problem.py
@@ -0,0 +1,16 @@
+def power(num, pwr):
+if pwr == 0:
+return 1
+else:
+return (num * power(num, pwr-1))
+
+def factorial(num):
+if num == 0:
+  return 1
+  else
+  return num * factorial(num - 1)
+
+print("5 to the power of 3 is {power(5, 3)}")
+print("2 to the power of 4 is {power(2,4)}")
+print("4! is {factorial (4)}")
+print("0! is {factorial (0)}")
