953789525b9af8643e26f51e258a03b366649d68
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-05 07:53:52 +0530

    Update 02_Problem.py

--- Practice_4/02_Problem.py
+++ Practice_4/02_Problem.py
@@ -9,7 +9,7 @@ if num == 0:
   return 1
   else 
   return num * factorial(num - 1)
-
+ 
 print("5 to the power of 3 is {power(5, 3)}")
 print("2 to the power of 4 is {power(2,4)}")
 print("4! is {factorial (4)}") 
