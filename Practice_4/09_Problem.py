b4550951d0106d65011c6276a4b2d5ed9b0e4afd
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-07 12:31:07 +0530

    Create 09_Problem.py

---
+++ Practice_4/09_Problem.py
@@ -0,0 +1,11 @@
+items = ["apple", "pear", "orange", "banana", "apple", "orange", "apple", "pear", "banana", "orange", "apple", "kiwi", "pear", "apple", "orange"]
+
+counter = dict()
+
+for items:
+if item in counter.keys():
+counter[item] += 1
+
+else:
+counter[item] = 1
+print(counter)
