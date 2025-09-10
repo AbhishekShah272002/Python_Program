7b34a44f28c3cfcdacd296c77108d3d2c2d1b0a1
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-10 20:07:24 +0530

    Update and rename 05_Problem to 05_Problem.py

---
+++ Practice_5/05_Problem.py
@@ -0,0 +1,16 @@
+user_preferences = {
+  "language": "English,
+  "font_size": "14px",
+"timezone": "USD",
+  "currency": False,
+  "volume_level": 80,
+  "date_formate": "MM/DD/YYYY"
+}
+
+user_preferences["language"] = "Spanish"
+
+user_preferences["volume_level"] = 50
+user_preference["highlight_color"] = "yellow"
+
+del user_perferences["currency"]
+user_preferences.pop("data_formate")
