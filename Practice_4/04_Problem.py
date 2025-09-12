b96df1856c1922a9a814dfd2f98989892b25673d
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-12 18:02:11 +0530

    Update 04_Problem.py

--- Practice_4/04_Problem.py
+++ Practice_4/04_Problem.py
@@ -1,7 +1,7 @@
  items = [20, 6, 8, 53, 56, 23, 87, 41, 49, 19]
 
 def quickSort(dataset, first, last):
-  if first < last:
+  if first < last: 
  
     pivotIdx = partition(dataset, first, last) 
  
