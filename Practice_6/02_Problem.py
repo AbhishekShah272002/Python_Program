4e3378688808cdca886c2e9272893603e877c679
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-28 11:55:57 +0530

    Update 02_Problem.py

--- Practice_6/02_Problem.py
+++ Practice_6/02_Problem.py
@@ -1,6 +1,6 @@
 lunch_menu=("Welcome Drink", "Veg Starter", "Non-Veg Stater","Veg Main Course","Non-Veg Main Course","Dessert")
 
-sample_tuple="A", "B", "C"
+sample_tuple="A", "B", "C" 
 sample_tuple1=("D",)
   
 print("Number of elements in the tuple, lunch_menu:",len(lunch_menu))
