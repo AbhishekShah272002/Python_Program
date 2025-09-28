096f781103da26c398adb7fd3725c6d50bc72b56
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-28 11:23:07 +0530

    Update 02_Problem.py

--- Practice_6/02_Problem.py
+++ Practice_6/02_Problem.py
@@ -9,5 +9,19 @@ print("Element of 2nd index position in lunch_menu:", lunch_menu[2])
 
 print("Concatenating tuples:")
 
-sample_tuple=sample_tuple+sample_tuple1
+sample_tuple = sample_tuple+sample_tuple1
 print(sample_tuple)
+
+sample_tuple = sample_tuple+("E",)
+print(sample_tuple)
+
+sample_tuple = sample_tuple+("F", "G")
+print(Sample_tuple)
+
+print("Iterating the tuple using range()")
+for index in range(0, len(lunch_menu)):
+  print(lunch_menu[index])
+
+print("Iterating the tuple using keyword in")
+for food_type in lunch_menu:
+  print(food_type)
