1e0ed3119ae8d0f8914a9c9c36590bea545da805
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-28 11:12:34 +0530

    Create 02_Problem.py

---
+++ Practice_6/02_Problem.py
@@ -0,0 +1,14 @@
+lunch_menu=("Welcome Drink", "Veg Starter", "Non-Veg Stater","Veg Main Course","Non-Veg Main Course","Dessert")
+
+sample_tuple="A", "B", "C"
+sample_tuple1=("D",)
+
+print("Number of elements in the tuple, lunch_menu:",len(lunch_menu))
+
+print("Element of 2nd index position in lunch_menu:", lunch_menu[2])
+
+print("Concatenating tuples:")
+
+sample_tuple=sample_tuple+sample_tuple1
+print(sample_tuple)
+
