2146331fa2db3a2b9309ea6ab7a50517acefe828
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-28 11:23:17 +0530

    Update 02_Problem.py

--- Practice_6/02_Problem.py
+++ Practice_6/02_Problem.py
@@ -9,7 +9,7 @@ print("Element of 2nd index position in lunch_menu:", lunch_menu[2])
 
 print("Concatenating tuples:")
 
-sample_tuple = sample_tuple+sample_tuple1
+sample_tuple = sample_tuple+sample_tuple1 
 print(sample_tuple)
 
 sample_tuple = sample_tuple+("E",)
