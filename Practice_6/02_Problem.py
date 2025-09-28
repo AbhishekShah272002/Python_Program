4a776ffea8c47721618a547c8f5692c357f50d07
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-28 11:29:48 +0530

    Update 02_Problem.py

--- Practice_6/02_Problem.py
+++ Practice_6/02_Problem.py
@@ -2,7 +2,7 @@ lunch_menu=("Welcome Drink", "Veg Starter", "Non-Veg Stater","Veg Main Course","
 
 sample_tuple="A", "B", "C"
 sample_tuple1=("D",)
- 
+  
 print("Number of elements in the tuple, lunch_menu:",len(lunch_menu))
 
 print("Element of 2nd index position in lunch_menu:", lunch_menu[2])
