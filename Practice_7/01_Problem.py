64953bf3539d575244446a9e602a9fdc6397050a
Abhishek Shah <abhishek27shah@gmail.com>
2025-09-30 11:39:24 +0530

    Update and rename 01_Problem to 01_Problem.py

---
+++ Practice_7/01_Problem.py
@@ -0,0 +1,10 @@
+pancard_number="AABGT6715H"
+
+print("Length of the PAN card number: ", len(pancard_number))
+
+name1 = "PAN"
+name2 = "card"
+name = name1+name2
+peint(name)
+
+print("Iterating the string using range()")
