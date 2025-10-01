6fcba07da1d9215ea7521092c84b57742d60ce3d
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-01 17:51:24 +0530

    Update 01_Problem.py

--- Practice_7/01_Problem.py
+++ Practice_7/01_Problem.py
@@ -4,7 +4,7 @@ print("Length of the PAN card number: ", len(pancard_number))
 
 name1 = "PAN"
 name2 = "card"
-name = name1+name2
+name = name1+name2 
 peint(name) 
 
 print("Iterating the string using range()")
