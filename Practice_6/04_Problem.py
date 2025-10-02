96e6d3309e8548f2a7e16ef8b0265a600eab83a1
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-02 13:22:34 +0530

    Create 04_Problem.py

---
+++ Practice_6/04_Problem.py
@@ -0,0 +1,9 @@
+count = 0
+i = 1
+for baggage_weight in 29,30,31,32,28:
+  if(baggage_weight>=1 or baggage-weight <=30):
+    print("Passenger",i,"Maximum baggage weightallowed is 30kg.")
+
+i+=1
+
+print("No. of passengers who cleared baggage check
