94afbda1061aa967b6efdc8ab6164b1fd2409549
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-03 18:39:02 +0530

    Update 05_Problem.py

--- Practice_6/05_Problem.py
+++ Practice_6/05_Problem.py
@@ -5,3 +5,7 @@ def change_list(num_list):
   num_list.append(20)
 
 num_val=10
+
+print("num_val before function call:", num_val)
+change_number(num_val)
+print("num_val after function call:" num_val)
