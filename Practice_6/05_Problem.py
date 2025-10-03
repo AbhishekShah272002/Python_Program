8031289f5fce2f3d904f9ada8c15cb4b7a169449
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-03 18:39:25 +0530

    Update 05_Problem.py

--- Practice_6/05_Problem.py
+++ Practice_6/05_Problem.py
@@ -9,3 +9,5 @@ num_val=10
 print("num_val before function call:", num_val)
 change_number(num_val)
 print("num_val after function call:" num_val)
+
+print("------------------------------------------------")
