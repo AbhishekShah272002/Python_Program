070b8a69e705af6002bba1c15b7497c9c567df60
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-04 08:16:59 +0530

    Create 04_Problem.py

---
+++ Practice_7/04_Problem.py
@@ -0,0 +1,8 @@
+def disply1(flight_number, seating_capacity):
+  print("Flight Number:", flight_number)
+  print("Seating Capacity:", seating_capacity)
+
+print("code-1: positional arguments")
+display1("AI789",200)
+
+def display2
