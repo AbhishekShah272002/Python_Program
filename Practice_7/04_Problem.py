c27d6d498d4a3de3d8c3a4393b15635b0718df22
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-04 08:23:59 +0530

    Update 04_Problem.py

--- Practice_7/04_Problem.py
+++ Practice_7/04_Problem.py
@@ -14,3 +14,6 @@ print("code-2: keyword arguments")
 display2(seating_capacity = 250, flight_number="AI789")
 
 def display3(flight_number, flight_make = "Boeing", seating_capacity = 150):
+  print("Flight Number:", flight_number)
+  print("Flight Make:", flight_make)
+  print("Seating Capacity:" seating_capacity)
