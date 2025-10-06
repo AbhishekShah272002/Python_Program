206ddbd566dcb54b6a39cd99d8d35fcf2cf2ed8a
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-06 07:56:09 +0530

    Create 05_Problem.py

---
+++ Practice_7/05_Problem.py
@@ -0,0 +1,12 @@
+wt_limit = 30
+
+def baggage_check(baggage_wt):
+  extra_baggage_charge=0
+  if not(baggage_wt>=0 and baggage_wt<=wt_limit):
+    extra_baggage=bggage_wt-wt_limit
+    extra_baggage_charge=extra_baggage*100
+    return extra_baggage_charge
+
+def update_baggage_limit(new_wt_limit):
+  wt_limit=new_wt_limit
+  print("This airline now
