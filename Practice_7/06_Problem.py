d53052eb225ec3fc33660253d8bf88e37ecb40ec
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-12 17:00:56 +0530

    Update 06_Problem.py

--- Practice_7/06_Problem.py
+++ Practice_7/06_Problem.py
@@ -1,7 +1,7 @@
 wt_limit = 30
 
 def baggage_check(baggage_wt):
-  extra_baggage_charge=0
+  extra_baggage_charge=0 
   if not(baggage_wt>=0 and baggage_wt<=wt_limit):
     extra_baggage=baggage_wt-wt_limit 
     extra_baggage_charge=extra_baggage*100 
