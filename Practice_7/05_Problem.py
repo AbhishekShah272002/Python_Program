75dc01029727a3d599bf1fc8bda0cb50f012c313
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-19 12:48:02 +0530

    Fix typo in baggage_check function

--- Practice_7/05_Problem.py
+++ Practice_7/05_Problem.py
@@ -3,7 +3,7 @@ wt_limit = 30
 def baggage_check(baggage_wt):
   extra_baggage_charge=0
   if not(baggage_wt>=0 and baggage_wt<=wt_limit):
-    extra_baggage=bggage_wt-wt_limit
+    extra_baggage=bggage_wt-wt_limit 
     extra_baggage_charge=extra_baggage*100
     return extra_baggage_charge
    
