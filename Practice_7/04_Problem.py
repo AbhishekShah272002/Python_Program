d27e9d07e7e38dd5a73f23ddd3822bdb52d8d585
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-20 16:25:46 +0530

    Update 04_Problem.py

--- Practice_7/04_Problem.py
+++ Practice_7/04_Problem.py
@@ -6,7 +6,7 @@ print("code-1: positional arguments")
 display1("AI789",200)
 
 def display2(flight_number, seating_capacity):  
-  print("Flight Number:", flight_number)
+  print("Flight Number:", flight_number) 
   print("Seating Capacity:", seating_capacity)   
 
 print("----------------------------------------------")
