df3aa471c33eca76820f297073396f5eb0df3bb2
Abhishek Shah <abhishek27shah@gmail.com>
2025-10-27 11:55:34 +0530

    Update 01_Problem.py

--- Practice_8/01_Problem.py
+++ Practice_8/01_Problem.py
@@ -5,7 +5,7 @@ if(airline=="AI"):
   elif(airline=="EM"): 
     max_wight=28  
     print(max_weight)
-    elif(airline=="BA"):
+    elif(airline=="BA"):  
       max_weight=35
       print(max_weight)  
  
