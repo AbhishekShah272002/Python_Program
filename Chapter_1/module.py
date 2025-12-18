d7a078e4a731cc2e9f4ee6fa71d09f6c86b40d5b
Abhishek Shah <abhishek27shah@gmail.com>
2025-12-18 08:16:25 +0530

    Update module.py

--- Chapter_1/module.py
+++ Chapter_1/module.py
@@ -2,7 +2,7 @@
 # import is used to include the pyjokes module in the script
 # The pyjokes module is a Python library that provides a collection of jokes
 import pyjokes
-
+ 
 # This is a single line comment 
 """ This is a multi-line comment"""
 
@@ -14,4 +14,5 @@ joke = pyjokes.get_joke()
 print(joke)
 
  
+
 
