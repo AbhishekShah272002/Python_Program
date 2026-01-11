18b1e4e51964871188d2b6c4a23d0b3358013c60
Abhishek Shah <abhishek27shah@gmail.com>
2026-01-11 12:32:38 +0000

    Update

--- Chapter_5/03.py
+++ Chapter_5/03.py
@@ -1,3 +1,4 @@
 # With the help of (CTRL KC) we can comment AND with the help of (CTRL KU) we can uncomment
 # With the help of string methods, write a program that checks whether the given input string is alphabetic or not.
 text = input("Enter a String: ")
+print("You entered: " + text) 
