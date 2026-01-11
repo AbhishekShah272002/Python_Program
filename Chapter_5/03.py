9d2d7d62083a7d43a3d88cb365464f44e47d2222
Abhishek Shah <abhishek27shah@gmail.com>
2026-01-11 14:16:55 +0000

    Update

--- Chapter_5/03.py
+++ Chapter_5/03.py
@@ -1,4 +1,5 @@
 # With the help of (CTRL KC) we can comment AND with the help of (CTRL KU) we can uncomment
 # With the help of string methods, write a program that checks whether the given input string is alphabetic or not.
 text = input("Enter a String: ")
-print("You entered: " + text) 
+print("You entered: " + text)
+print("Is the entered string alphabetic? " + str(text.isalpha()))
