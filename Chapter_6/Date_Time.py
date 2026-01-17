f2e0805c171b8e52b1151abc72abcaf56f25164c
Abhishek Shah <abhishek27shah@gmail.com>
2026-01-17 02:39:52 +0000

    Updte the Date_Time

--- Chapter_6/Date_Time.py
+++ Chapter_6/Date_Time.py
@@ -1,4 +1,9 @@
-from datetime import datetime
+#To get current DATE AND TIME
+from datetime import datetime, timedelta
 today = datetime.now()
 
 print('Today: ' + str(today))
+
+one_day = timedelta(days=1)
+yesterday = today - one_day
+print('Yesterday ' + str(yesterday))
